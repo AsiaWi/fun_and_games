c356d46a05318edba604a0f6ae1b0cb6bf0590bb
AsiaWi <joannawitek@icloud.com>
2023-09-08 21:34:31 +0000

    Correct typo in settings file

--- fun_and_games/settings.py
+++ fun_and_games/settings.py
@@ -46,8 +46,8 @@ INSTALLED_APPS = [
     'playground',
 
     #Other
-    'cloudinary_storage'
-    'cloudinary'
+    'cloudinary_storage',
+    'cloudinary',
 ]
 
 MIDDLEWARE = [
@@ -129,14 +129,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_STORAGE= 'cloudinary_storage.storage.StaticHashedCloudinaryStorage'
-STATICFILES_DIR= [os.path.join(BASE_DIR, 'static')]
-STATIC_ROOT= os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = 'cloudinary_storage.storage.StaticHashedCloudinaryStorage'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-MEDIA_URL= '/media/'
-DEFAULT_FILE_STORAGE= 'cloudinary_storage.storage.MediaCloudinaryStorage'
-
-
+MEDIA_URL = '/media/'
+DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
