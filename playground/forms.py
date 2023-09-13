848b3cf9bbc07b99fdc879cde46a3945b4d4097b
AsiaWi <joannawitek@icloud.com>
2023-09-13 20:40:40 +0000

    Add form class ready to create Activity view

---
+++ playground/forms.py
@@ -0,0 +1,21 @@
+from .models import Activity
+from django import forms
+from djrichtextfield.widgets import RichTextWidget
+
+
+class ActivityForm(forms.ModelForm):
+    
+    class Meta:
+        model = Activity
+        fields = ['title', 'excerpt', 'featured_image', 'image_alt', 'content', 'link', 'privacy']
+        content = forms.CharField(widget=RichTextWidget())
+
+        labels = {
+            'title': 'Activity Title',
+            'excerpt': 'Post Teaser',
+            'featured_image': 'Add Image',
+            'image_alt': 'Image Description',
+            'content': 'Describe Activity',
+            'link': 'Can you leave a website link?',
+            'privacy': 'Publish post?'
+        }
