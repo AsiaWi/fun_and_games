8094aea6646a72b333d5b8b34f31fff26111809a
AsiaWi <joannawitek@icloud.com>
2023-09-17 23:39:14 +0000

    Create display profile wall view and profile_wall html file for user to view own posts only

--- playground/urls.py
+++ playground/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from .views import DisplayActivityList, AddActivity
+from .views import DisplayActivityList, AddActivity, DisplayActivityDetails, DisplayProfileWall
 
 
 # path list
 urlpatterns= [
     path('', DisplayActivityList.as_view(), name = 'index'),
-    path('add/', AddActivity.as_view(), name = 'add_activity')
+    path('profile/', DisplayProfileWall.as_view(), name = 'profile_wall'),
+    path('add/', AddActivity.as_view(), name = 'add_activity'),
+    path('<slug:pk>/', DisplayActivityDetails.as_view(), name = 'view_activity_details'),
 ]
--- playground/views.py
+++ playground/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView, CreateView, ListView
+from django.views.generic import TemplateView, CreateView, ListView, DetailView
 from .models import Activity
 from django.contrib.auth.mixins import LoginRequiredMixin
 from .forms import ActivityForm
@@ -27,3 +27,12 @@ class DisplayActivityList(ListView):
     paginate_by = 3
     context_object_name = 'activities'
 
+class DisplayProfileWall(ListView):
+    model = Activity
+    template_name = 'playground/profile_wall.html'
+    context_object_name = 'activities'
+
+class DisplayActivityDetails(DetailView):
+    model = Activity
+    template_name = 'playground/view_activity_details.html'
+    context_object_name = 'activity'
