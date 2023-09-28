27883afda247d914a7e57eff38d3a94a16b1cc60
AsiaWi <joannawitek@icloud.com>
2023-09-28 20:53:33 +0000

    Debug set to false to test deployed version

--- fun_and_games/settings.py
+++ fun_and_games/settings.py
@@ -29,7 +29,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 RICHTEXTFIELD_KEY = os.environ.get('RICHTEXTFIELD_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['8000-asiawi-funandgames-0ypziizicl3.ws-eu104.gitpod.io', 'fun-and-games-a99303d46c12.herokuapp.com', '8000-asiawi-funandgames-0ypziizicl3.ws-eu105.gitpod.io']
 
