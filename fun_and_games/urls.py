da2ad58877ebeb67b4ad4a962ca1bd2eb59598d3
AsiaWi <joannawitek@icloud.com>
2023-10-11 19:39:06 +0000

    Add active class to all links in nav

--- fun_and_games/urls.py
+++ fun_and_games/urls.py
@@ -19,7 +19,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('accounts/', include('allauth.urls')),
+    path('accounts/', include('allauth.urls'), name='allauth'),
     path('', include('playground.urls')),
     path('playground/', include('playground.urls'))
 ]
